3a35fcb2723a0575fe00ceb6ab7dcf8072bc6331
Bilèle El Haddadi <bilele.elhaddadi@etu.inp-n7.fr>
2025-12-10 11:55:22 +0100

    feat: ajout 2 modules, fichier et routage

--- routage.ads
+++ routage.ads
@@ -1,8 +1,33 @@
-with LCA ;
+with LCA;
 with Ada.Strings.Unbounded ;    use Ada.Strings.Unbounded ;  
 
 package Routage is 
-	type Routage is limited private 
-	
-	function  Interface (Strings.Unbounded ip    ) return
-	function Est_Valide (  Strings.Unbounded ip              )  return  Boolean ;  	
+
+    type Route is limited private;
+
+    package LCA_Routage is
+        new LCA (T_Element => Route);
+    use LCA_Routage;
+
+    type Table_Routage is LCA_Routage.LCA;
+
+    function Get_Interface (Unbounded_String ip, Table_Routage table) return Unbounded_String;
+
+    function Est_Valide (IP_Adresse ip, Route route) return Boolean ;  	
+
+    function Charger_Table_Routage(File_Type file) return Table_Routage;
+
+    function String_Vers_Ip(Unbounded_String ip) return IP_Adresse;
+
+
+private:
+
+    type IP_Adresse is mod 2**32;
+    type Route is
+        record
+            Ip : Ip_Adresse;
+            Masque : Ip_Adresse;
+            Interface_Route : Unbounded_String;
+        end record;
+
+end Routage;
