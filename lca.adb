0aa5cd829c225a1a8c6bbfcbc667d166960825db
Bilèle El Haddadi <bilele.elhaddadi@etu.inp-n7.fr>
2025-12-19 14:35:42 +0100

    refactor: LCA code indentation, formattage, corretions

--- lca.adb
+++ lca.adb
@@ -1,38 +1,40 @@
-with Ada.Text_IO;            use Ada.Text_IO;
+with Routage_Exceptions;    use Routage_Exceptions;
 
 package body LCA is 
 
-     procedure Initailiser(LCA :out T_Pile )is
-     begin
-	     LCA:=null;
-     end Initialiser;
+    procedure Initialiser(LCA : out T_LCA) is
+    begin
+        LCA := null;
+    end Initialiser;
 
 
-     function Est_Vide (LCA : T_Pile) return Boolean is
-     begin
-	     return LCA = null ;
-     end Est_vide ;
-    
+    function Est_Vide (LCA : T_LCA) return Boolean is
+    begin
+        return LCA = null;
+    end Est_vide;
 
-     procedure Enregistrer_Recursif (LCA : in out T_Pile;  Element : in T_Element) is 
-     begin 
-          if LCA = null then 
-        	LCA : = new T_Pile'(
-	        Element => Element,
-	        Suivant => null);
-       	elsif LCA.Element=Element then 
-	   LCA.Element:= Element ;
+
+    procedure Enregistrer_Recursif (LCA : in out T_LCA; Element : in T_Element) is 
+        nouvelle_LCA : T_LCA;
+    begin 
+        if Est_Vide(LCA) then 
+            nouvelle_LCA := new T_Cellule;
+            nouvelle_LCA.Element := Element;
+            nouvelle_LCA.Suivant := null;
+            LCA := nouvelle_LCA;
+        elsif LCA.Element = Element then 
+            raise Duplicate_Route_Error;
         else 
-           Enregistrer_Recursif(LCA.Suivant , Element ) ;
-        end if ;
-    end Enregistrer_Recursif ;
+            Enregistrer_Recursif(LCA.Suivant, Element);
+        end if;
+    end Enregistrer_Recursif;
 
 
     function Premier(LCA : in T_LCA) return T_LCA is
     begin
         return LCA;
     end Premier;
-	           	   
+
 
     function Element (LCA : in T_LCA) return T_Element is
     begin
@@ -45,7 +47,4 @@ package body LCA is
         return LCA.Suivant;
     end Suivant;
 
- end LCA ;
-
-	     
-
+end LCA;
